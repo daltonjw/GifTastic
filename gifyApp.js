cbbe68c7bd7d4cf73f0c80bf151543edacea7584
daltonjw <dalton.jw@gmail.com>
2016-12-04 21:00:34 -0600

    cleaned up comments and removed extra files

--- gifyApp.js
+++ gifyApp.js
@@ -1,9 +1,9 @@
-
-var topics = ["fish", "birds"];
+// Initialinzing variables. Topics array for creating initial search buttons...as chosen by my 3 year ol 
+var topics = ["dinosaurs", "toys", "buckets"];
 
 var authKey = "dc6zaTOxFJmzC";
-// var queryURLBase = "http://api.giphy.com/v1/gifs/search?q=" +  + "&api_key=dc6zaTOxFJmzC";
-var searchTerm = "";
+
+// Variable to set number of gifs to return
 var numResults = 10;
 
 
@@ -12,15 +12,12 @@ function renderButtons()
 
 		// Empties the div prior to adding new buttons (this is necessary otherwise you will have repeat buttons)
 		$("#buttonsView").empty();
-		// var tempArray = totalArrayAnswers[n];
-		// console.log(tempArray);
-		// Loops through the array of answers
+		// Loops through the array of topics
 		for (var i = 0; i < topics.length; i++)
 		{
 
 			// Then dynamicaly generates buttons for each item in the array
 
-			// Note the jQUery syntax here... 
 		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
 		    a.addClass('gifbutton'); // Added a class 
 		    a.attr('data-name', topics[i]); // Added a data-attribute
@@ -39,25 +36,25 @@ function renderButtons()
 		// The movie from the textbox is then added to our array
 		topics.push(newGif);
 		
-		// Our array then runs which handles the processing of our movie array
+		// Our function then runs which handles the processing of our buttons/topics array
 		renderButtons();
 
-		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		// We have this line so that users can hit "enter" instead of clicking on submit button and it won't move to the next page
 		return false;
 	})
 
-		// displayMovieInfo function now re-renders the HTML to display the appropriate content. 
+		// displayGif function now re-renders the HTML to display the appropriate content. 
 	function displayGif(){
 
 		var gif = $(this).attr('data-name');
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&limit=" + numResults + "&api_key=" + authKey;
 		
-		// Creates AJAX call for the specific movie being 
+		// Creates AJAX call for the specific gif/button being 
 		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 
 			console.log(response);
 
-			// // Creates a generic div to hold the movie
+			// // Creates a generic div to hold the gif(s)
 			var gifDiv = $('<div class="gif">');
 
 			for (i = 0; i < numResults; i++) 
@@ -68,10 +65,10 @@ function renderButtons()
 				// Creates an element to have the rating displayed
 				var pOne = $('<p>').text( "Rating: " + rating);
 
-				// Displays the rrating
+				// Displays the rating
 				gifDiv.append(pOne);
 
-				// // Creates an element to hold the image 
+				// // Creates an element to hold the gif image 
 				var image = $('<img>')
 				image.attr("src", response.data[i].images.fixed_height_still.url);
 				image.attr("data-still", response.data[i].images.fixed_height_still.url);
@@ -84,27 +81,7 @@ function renderButtons()
 			}
 
 
-			// // Retrieves the release year
-			// var released = response.Released;
-
-			// // Creates an element to hold the release year
-			// var pTwo = $('<p>').text( "Released: " + released);
-
-			// // Displays the release year
-			// gifDiv.append(pTwo);
-
-			// // Retrieves the plot
-			// var plot = response.Plot;
-
-			// // Creates an element to hold the plot
-			// var pThree = $('<p>').text( "Plot: " + plot);
-
-			// // Appends the plot
-			// gifDiv.append(pThree);
-
-
-
-			// Puts the entire Movie above the previous movies.
+			// Puts the entire set of gifs above the previous gifs.
 			$('#gifView').prepend(gifDiv);
 		});
 
@@ -112,28 +89,17 @@ function renderButtons()
 
     function changeState() 
     {
-      // STEP ONE: study the html above.
-      // Look at all the data attributes.
-      // Run the file in the browser. Look at the images.
-
-      // After we complete steps 1 and 2 we'll be able to pause gifs from giphy.
-
-      // STEP TWO: make a variable named state and then store the button's data-state into it.
-      // Do not use .data('state'). It won't work the way we expect.
-
-      // ============== FILL IN CODE HERE FOR STEP TWO =========================
+      // defining a variable state and assigining data-state attribute 
       var state=$(this).attr("data-state");
 
-      // =============================================
-
-      // STEP THREE: Check if the variable state is equal to 'still',
+      // Check if the variable state is equal to 'still',
       if (state == "still"){
           $(this).attr("src", $(this).attr("data-animate"));
           $(this).attr("data-state", "animate");
 
       }
-      // then update the src attribute of this image to it's data-animate value,
 
+      // then update the src attribute of this image to it's data-animate value,
       // and update the data-state attribute to 'animate'.
 
       if (state != "still"){
@@ -142,18 +108,11 @@ function renderButtons()
       }
       // If state does not equal 'still', then update the src attribute of this
       // image to it's data-animate value and update the data-state attribute to 'still'
-      // ============== FILL IN CODE HERE FOR STEP THREE =========================
-
-      // CODE GOES HERE
-
-      // ==============================================
-
-      // STEP FOUR: open the file in the browser and click on the images.
-      // Then click again to pause.
     };
 
 renderButtons();
 
-// Generic function for displaying the movieInfo
+// Generic function for displaying the gifs
 $(document).on('click', '.gifbutton', displayGif);
+// Generic function for changing animation state of gifs when clicked on
 $(document).on('click', '.gifImage', changeState);
